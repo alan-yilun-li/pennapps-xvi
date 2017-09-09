c9e837af15dc04ffffe7dfc8d71a3a55bc37a343
Alan Li <alan.yilun.li@gmail.com>
2017-09-09 17:33:42 -0400

    Separated client and server on repo to folders

---
+++ client/pennapps-xvi/Networking/Secrets/APIKeys.swift
@@ -0,0 +1,19 @@
+//
+//  APIKeys.swift
+//  pennapps-xvi
+//
+//  Created by Alan Li on 2017-09-09.
+//  Copyright © 2017 Alan Li. All rights reserved.
+//
+
+import Foundation
+
+/// Static structure for storing API Keys
+struct APIKeys {
+    
+    
+    
+    
+    
+    
+}

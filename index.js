92a73b2bd0a014b03348a79db78ad204cc2109a7
mokaixu <mokaixu@gmail.com>
2017-09-10 07:37:27 -0400

    updated server

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 
-app.listen(8000, function () {
-  console.log('Example app listening on port 8000!')
+app.listen(process.env.PORT || 5000, function() {
+    console.log("hello");
 })

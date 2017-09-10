9f7916055aabac28dc0cfd57d19c4d04badf8008
Alan Li <alan.yilun.li@gmail.com>
2017-09-10 01:31:35 -0400

    Added swifty JSON

---
+++ client/pennapps-xvi/Networking/APIKeys.swift
@@ -0,0 +1,16 @@
+//
+//  APIKeys.swift
+//  pennapps-xvi
+//
+//  Created by Alan Li on 2017-09-10.
+//  Copyright © 2017 Alan Li. All rights reserved.
+//
+
+import Foundation
+
+class APIKeys {
+    
+    static let GOOGLE_API_KEY = "AIzaSyDNPYaevc0c2R9rl9LqL5UGt-mGoykLzow"
+    
+
+}
